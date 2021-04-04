3a6a957b29953fdd0fc1768165844fd5ff017e9e
Zac Sanders <zacharyjsanders@gmail.com>
2021-04-04 10:48:38 -0600

    Adds featured image to posts

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Router, Route, NavLink } from "react-router-dom";
+import { createBrowserHistory } from 'history'
+import Home from "./components/views/home";
+import About from "./components/views/about";
+import Contact from "./components/views/contact";
+import Photography from "./components/views/photography";
+
+class Main extends Component {
+  render() {
+    return (
+      <Router history={createBrowserHistory()}>
+        <Route path='/' component={Home}>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/photography">Photography</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/photography" component={Photography}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+        </Route>
+      </Router>
+    );
+  }
+}
+ 
+export default Main;
